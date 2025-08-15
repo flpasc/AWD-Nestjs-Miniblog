3905270c5e7824fc734ec9bb4d0ebb2fbfc707f2
Florian <florianpaulschoene@gmail.com>
2025-08-15 11:58:10 +0200

    header link color

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <nav style={{ backgroundColor: "blue" }}>
+      <nav style={{ backgroundColor: "blue", color: "white" }}>
         <Link href={"/"}>Home</Link>
       </nav>
     </header>
